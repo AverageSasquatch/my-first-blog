d8e8a39ef6e8738a5da389c4301dd62743a46cea
Jake Serdar <huntingbuddy419@gmail.com>
2021-09-07 19:28:41 -0600

    Fix allowed hosts in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nzwsen3*he0jg6ma$b154!oq!6q4z@)njxgl0rkf_)f_t2oz55'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'averagesasquatch.pythonanywhere.com']
 
 
 # Application definition
